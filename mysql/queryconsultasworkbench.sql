1fac47d10d2adcbe2ece3f8612e12f9de42c83d6
Jose Antonio Nicolas Carbajal <antoniocarbajal14@hotmail.com>
2023-02-15 16:42:08 -0600

    Primer commit de proyecto inicial

---
+++ mysql/queryconsultasworkbench.sql
@@ -0,0 +1,17 @@
+-- listar las oficinas cuyas ventas estan por debajo del 80% de sus objetivos
+
+use bdeejemplo;
+
+select * from oficinas;
+
+select oficina, ciudad, region, ventas, objetivo
+from Oficinas
+where Ventas < (0.80 * Objetivo);
+
+-- hallar los pedidos del ultimo trimestre de 1989
+
+select * from Pedidos 
+where Fecha_Pedido between '1989-10-01' and '1989-12-31';
+
+select * from Pedidos 
+where Fecha_Pedido >= '1989-10-01' and Fecha_Pedido <= '1989-12-31';
